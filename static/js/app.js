321a3d40b85e604ffa0360b8d8072a69a9b06765
Jordan Cline <jordancline@Jordans-MacBook-Pro.local>
2020-11-13 21:17:14 -0500

    barchart function added.

--- static/js/app.js
+++ static/js/app.js
@@ -30,6 +30,38 @@ function demographics(id) {
         demo.append("h5").text('${key}: ${value)'));
 };
 
+//barchart
+function barchart(id) {
+    var sample = jsonData.samples.filter(x => x.id.toString() === id)[0];
+    //console.log(sample)
+
+    //top 10 otus and their ids after mapping
+    var topten = sample.otu_ids.slice(0, 10).reverse();
+    var otuIDs = topten.map(x => "OTU " + x);
+
+    // values and labels
+    var values = sample.sample_values.slice(0, 10).reverse();
+    var labels = sample.otu_labels.slice(0, 10).reverse();
+
+    //trace
+    var trace1 = [{
+        type: "bar",
+        x: values,
+        y: otuIDs,
+        text: labels,
+        //ensures horizontal
+        orientation: 'h'
+    }];
+    //layout
+    var layout1 = {
+        title: "Top 10 OTU Samples",
+        xaxis: {title: "Sample Values"},
+        yaxis: {title: "OTU ID"}
+    };
+
+    Plotly.newPlot('bar', trace1, layout1)
+};
+
 
 
 //this will be used to activate functions
