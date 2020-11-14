3d730da1699cf69a1128379478ff76b136e7887f
Jordan Cline <jordancline@Jordans-MacBook-Pro.local>
2020-11-13 21:00:08 -0500

    adding dropdown function, demographic function, and active function which will be needed.

--- static/js/app.js
+++ static/js/app.js
@@ -10,3 +10,33 @@ d3.json(jsonPath).then((data) => {
     //initiatlization
     init();
 });
+
+
+//dropdown function
+function init() {
+    var dropdown = d3.select('#selDataset');
+    jsonData.names.forEach((name) =>{
+        dropdown.append('option').text(name).property('value',name);
+    });
+    active(jsonData.names[0])
+};
+
+//demographic function
+function demographics(id) {
+    var meta = jsonData.metadata.filter((x) => x.id === parseInt(id))[0];
+    var demo = d3.select("#sample-metadata");
+    demo.html("");
+    Object.entries(meta).forEach(([key,value]) =>
+        demo.append("h5").text('${key}: ${value)'));
+};
+
+
+
+//this will be used to activate functions
+
+function active(id) {
+    //demo stuff, bar chart, and bubble chart
+    demographics(id);
+    barchart(id);
+    bubblechart(id);
+}
